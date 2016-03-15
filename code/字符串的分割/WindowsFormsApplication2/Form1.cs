1ce50f883f0f2897d9f22841d8105a61bd814b51
bfchengnuo <bfchengnuo@gmail.com>
2016-03-15 21:45:53 +0800

    update3字符串

---
+++ code/字符串的分割/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = System.DateTime.Now.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //指定分割顺序
+            string[] ss = label1.Text.Split('-');
+            string[] s1 = ss[2].Split(':');
+            string[] s2 = s1[0].Split(' ');
+            for (int i = 0; i < 2; i++)
+            {
+                MessageBox.Show(ss[i]);
+            }
+            MessageBox.Show(s2[0]);
+            MessageBox.Show(s2[1]);
+
+            MessageBox.Show(s1[1]);
+            MessageBox.Show(s1[2]);
+            /*
+            for (int i = 1; i < 3;i++ )
+            {
+                MessageBox.Show(s1[i]);
+            }
+             */
+
+        }
+    }
+}
---
+++ code/字符串的截取/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label4.Text = textBox1.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //第二个数字是长度，不是下标！
+            string s1 = textBox1.Text.Substring(0, Convert.ToInt32(textBox2.Text));
+            //string s1 = textBox1.Text.Substring(0, int.Parse(textBox2.Text));
+            
+            string s2 = textBox1.Text.Substring(Convert.ToInt32(textBox2.Text));
+            label4.Text = s1 + textBox3.Text + s2;
+        }
+    }
+}
